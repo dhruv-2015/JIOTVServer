5fd299a42969a3da20359af1a6b4585d1c57bd85
Dhruv_Patle <79126430+dhruv-2015@users.noreply.github.com>
2023-02-04 18:49:15 +0530

    Update install.sh

--- V2.0.3/install.sh
+++ V2.0.3/install.sh
@@ -1,1 +1,76 @@
+#!/bin/bash
 
+# This scripts perform installation of required dependencies on termux
+
+# Install nodejs
+pkg update
+
+if [ $? -eq 0 ]; then
+   echo "update successful"
+else
+    echo "Failed to update. Exiting"
+    exit 1
+fi
+
+pkg install nodejs-lts -y
+
+if [ $? -eq 0 ]; then
+   echo "Installation successful"
+else
+    echo "Failed to install nodejs. Exiting"
+    exit 1
+fi
+
+# Get the latest release of code
+curl https://github.com/dhruv-2015/JIOTVServer/releases/download/V2.0.3/JTVServer.zip -o JTVServer.zip
+
+
+if [ $? -eq 0 ]; then
+   echo "Downloaded the server successfully. "
+else
+    echo "Failed to download the server. Is wget installed?"
+    exit 1
+fi
+
+# Unzip the downloaded file
+unzip JTVServer.zip
+
+if [ $? -eq 0 ]; then
+   echo "File unzipped"
+else
+    echo "Failed to unzip the file"
+    exit 1
+fi
+
+rm JTVServer.zip
+
+if [ $? -eq 0 ]; then
+   echo "Deleted source"
+else
+    echo "Failed to delete the source"
+fi
+
+cd JTVServer
+if [ $? -eq 0 ]; then
+   echo "Opened source"
+else
+    echo "Failed to open source. Is the directory present ? Exiting"
+    exit 1
+fi
+
+
+echo "Downloading start server script"
+cd ~
+curl -o start.sh https://raw.githubusercontent.com/dhruv-2015/JIOTVServer/main/V2.0.3/start.sh
+if [ $? -eq 0 ]; then
+   echo "Script downloaded successfully"
+else
+    echo "Failed to download start script. Please run wget https://raw.githubusercontent.com/dhruv-2015/JIOTVServer/main/V2.0.3/start.sh to download"
+    exit 1
+fi
+
+cd ~
+echo "==========================================================="
+echo "INSTALLATION COMPLETED"
+echo "RUN SERVER START SCRIPT TO START THE SERVER"
+echo "==========================================================="
