834d29c8c02fc9f15e1f166f5c66043852ecf170
Dhruv_Patle <chadasaniyadhruv@gmail.com>
2023-08-26 15:26:44 +0530

    Fix some bugs

--- utils/refreshToken.mjs
+++ utils/refreshToken.mjs
@@ -1,30 +1,30 @@
 import fetch from "node-fetch";
 import fs from "fs";
-var options = {
-  method: "POST",
-  headers: {
-    devicetype: "phone",
-    versionCode: "290",
-    os: "android",
-    Connection: "close",
-    "Content-Type": "application/json; charset=utf-8",
-    "Content-Length": "108",
-    Host: "auth.media.jio.com",
-    "Accept-Encoding": "gzip",
-    "User-Agent": "okhttp/4.2.2",
-  }
-};
-
 export default async function genNewAccessToken() {
   try {
     let userDataJiotv = JSON["parse"](
       fs["readFileSync"]("tokenData.jiotv", { encoding: "utf8", flag: "r" })
     );
-    options["body"] = JSON.stringify({
-      appName: "RJIL_JioTV",
-      deviceId: "3c6d6b5702fa09bd",
-      refreshToken: userDataJiotv["refreshToken"],
-    });
+    let options = {
+      method: "POST",
+      headers: {
+        devicetype: "phone",
+        versionCode: "290",
+        os: "android",
+        Connection: "close",
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Length": "108",
+        Host: "auth.media.jio.com",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/4.2.2",
+        accessToken: userDataJiotv["authToken"],
+      },
+      body: JSON.stringify({
+        appName: "RJIL_JioTV",
+        deviceId: "3c6d6b5702fa09bd",
+        refreshToken: userDataJiotv["refreshToken"],
+      }),
+    };
     let response = await fetch(
       "https://auth.media.jio.com/tokenservice/apis/v1/refreshtoken?langId=6",
       options
